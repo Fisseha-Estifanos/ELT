abf210cc1e2cc5128bd5a3563ae0f3e6c0f9ef91
Fisseha Linux <fisseha.137@gmail.com>
2022-09-20 02:21:17 +0300

    name error ifx

--- dags/postgre-dag.py
+++ dags/postgre-dag.py
@@ -20,28 +20,32 @@ with DAG(
     schedule="@once",
     catchup=False,
 ) as dag:
-    """create_pet_table = PostgresOperator(
-        task_id="create_pet_table",
-        sql="""
-            # CREATE TABLE IF NOT EXISTS pet (
-            # pet_id SERIAL PRIMARY KEY,
-            # name VARCHAR NOT NULL,
-            # pet_type VARCHAR NOT NULL,
-            # birth_date DATE NOT NULL,
-            # OWNER VARCHAR NOT NULL);
-          # """,
-    # )"""
-
     create_pet_table = PostgresOperator(
         task_id="create_pet_table",
         postgres_conn_id="postgres_default",
-        sql="../sql/pet_schema.sql",
+        # sql="../sql/pet_schema.sql",
+        sql="""
+        -- create pet table
+        CREATE TABLE IF NOT EXISTS pet (
+        pet_id SERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        pet_type VARCHAR NOT NULL,
+        birth_date DATE NOT NULL,
+        OWNER VARCHAR NOT NULL);
+        """,
     )
 
     populate_pet_table = PostgresOperator(
         task_id="populate_pet_table",
         postgres_conn_id="postgres_default",
-        sql="../sql/populate_pet_schema.sql",
+        # sql="../sql/populate_pet_schema.sql",
+        sql="""
+        -- populate pet table
+        INSERT INTO pet VALUES ( 'Max', 'Dog', '2018-07-05', 'Jane');
+        INSERT INTO pet VALUES ( 'Susie', 'Cat', '2019-05-01', 'Phil');
+        INSERT INTO pet VALUES ( 'Lester', 'Hamster', '2020-06-23', 'Lily');
+        INSERT INTO pet VALUES ( 'Quincy', 'Parrot', '2013-08-11', 'Anne');
+        """,
     )
 
     get_all_pets = PostgresOperator(
@@ -58,3 +62,4 @@ with DAG(
     )
 
 create_pet_table >> populate_pet_table >> get_all_pets >> get_birth_date
+# sudo -u postgres psql postgres
