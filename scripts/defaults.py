c42e1026486e55bd0ce2958a7eccbff95d5a7eb9
Fisseha Linux <fisseha.137@gmail.com>
2022-09-20 17:28:54 +0300

    adding defaults

---
+++ scripts/defaults.py
@@ -0,0 +1,22 @@
+"""
+A default script for all default variables
+"""
+
+# base repository
+repo = '../'
+
+# the path to the data folder
+data_path = 'data/'
+
+# setting up dvc data and folder structure paths
+# main data file name
+data_file_name = '20181024_d1_0830_0900.csv'
+
+# the local data path
+full_data_file = repo + data_path + data_file_name
+
+# the path to the data set
+data_file = data_path + data_file_name
+
+# the path to the plots folder
+plot_path = 'plots/'
